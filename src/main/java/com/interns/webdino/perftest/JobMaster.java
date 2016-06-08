a158a503d40cc9b6902435b310453ceb343288d9
matthewLee711 <matthew.lee711@gmail.com>
2016-06-08 15:56:35 -0500

    unimportant

--- src/main/java/com/interns/webdino/perftest/JobMaster.java
+++ src/main/java/com/interns/webdino/perftest/JobMaster.java
@@ -6,21 +6,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class JobMaster {
-
+	//Store all jobs
     private HashMap<String,Job> jobs = new HashMap<>();
-
+    //return job name
     public Job getJob(String name){
         return jobs.get(name);
     }
-
+    //add jobs to hash map
     public void addJob(Job job){
         jobs.put(job.getName(), job);
     }
-
+    //remove jobs from hash map
     public void removeJob(String name){
         jobs.remove(name);
     }
-
+    //execute job
     public void runJob(String name){
 
         Job job = jobs.get(name);
@@ -29,7 +29,7 @@ public class JobMaster {
             job.run();
         }
     }
-
+    //Tells status of job
     public JobStatus getJobStatus(String name){
 
         Job job = jobs.get(name);
